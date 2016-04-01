1aa1f5837086a7e6ecdbae0f9a7ff762ea7f9c8c
1C3 <riccardo@ice.ovh>
2016-04-01 03:21:40 +0200

    Create main activity

--- app/src/main/java/ovh/ice/icecons/MainActivity.java
+++ app/src/main/java/ovh/ice/icecons/MainActivity.java
@@ -1,7 +1,10 @@
 package ovh.ice.icecons;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void gitLink(View v) {
+        Uri uri = Uri.parse("https://github.com/1C3/ICEcons");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
     }
 }
