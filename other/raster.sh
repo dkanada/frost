9fb5e65edb1a6e3684c643a69913326aadcae44e
dkanada <dkanada@users.noreply.github.com>
2017-06-02 16:29:16 -0600

    fix openlauncher icon

--- other/raster.sh
+++ other/raster.sh
@@ -8,6 +8,9 @@ for DIR in $(find -name "*.svg")
 do
   FILE=${DIR##*/}
   NAME=${FILE%.*}
+  cp ${FILE} ${FILE}.tmp
+  scour --remove-descriptive-elements --enable-id-stripping --enable-viewboxing --enable-comment-stripping --nindent=4 -i ${FILE}.tmp -o ${FILE}
+  rm ${FILE}.tmp
   cp -f $FILE ${ICON}/$FILE
   for SIZE in $SIZES
   do
