f34dd6c7737f5383adc6bd268a804a713834a77b
dkanada <dkanada@users.noreply.github.com>
2017-11-05 04:11:43 -0700

    static array of icons is now automatically generated from xml file

--- app/src/main/java/com/dkanada/icecons/async/BitmapWorkerTask.java
+++ app/src/main/java/com/dkanada/icecons/async/BitmapWorkerTask.java
@@ -1,45 +1,46 @@
 package com.dkanada.icecons.async;
 
+import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.view.View;
 import android.widget.ImageView;
 
-import java.lang.ref.WeakReference;
-
 import com.dkanada.icecons.utils.IceImageUtils;
 
 public class BitmapWorkerTask extends AsyncTask<Integer, Void, Bitmap> {
 
-    private WeakReference<ImageView> imageViewReference;
-    public Resources res;
-    public int resId;
+    private ImageView imageView;
+    public Context context;
+    public Resources resources;
+    public String image;
     public int width;
     public int height;
 
     public BitmapWorkerTask(ImageView imageView) {
-        imageViewReference = new WeakReference<>(imageView);
+        this.imageView = imageView;
     }
 
     // decode image in background
     @Override
     protected Bitmap doInBackground(Integer... parameters) {
-        return IceImageUtils.bitmapLoad(res, resId, width, height);
+        int id = resources.getIdentifier(image, "drawable", context.getPackageName());
+        return IceImageUtils.bitmapLoad(resources, id, width, height);
     }
 
     // check if image view still exists and set bitmap
     @Override
     protected void onPostExecute(Bitmap bitmap) {
-        if (imageViewReference != null && bitmap != null) {
-            final ImageView imageView = imageViewReference.get();
+        if (imageView != null && bitmap != null) {
+            ImageView imageView = this.imageView;
             if (imageView != null) {
                 imageView.setImageBitmap(bitmap);
                 imageView.setAlpha(0f);
                 imageView.setVisibility(View.VISIBLE);
                 imageView.animate()
                         .alpha(1f)
-                        .setDuration(res.getInteger(android.R.integer.config_mediumAnimTime))
+                        .setDuration(resources.getInteger(android.R.integer.config_mediumAnimTime))
                         .setListener(null);
             }
         }
